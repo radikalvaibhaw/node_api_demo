6d9e592eaeadacf00b0b2d04e9568e3869018f59
Patit <patit@isisdsn.net>
2018-11-21 14:29:54 +0530

    Updated

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ var login = require('./loginModel/login');
 var bodyParser = require('body-parser');
 
 var app=express();
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.urlencoded({extended: true,limit: '50mb', parameterLimit:50000}));
+app.use(cookieParser());
 
-app.use(bodyParser.json({limit: '5mb'}));
-app.use(bodyParser.urlencoded({ extended: true ,limit: '5mb'}));
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
+  next();
+};
+
+app.use(allowCrossDomain);
 app.get('/',function(req,res)
 {
 res.send('Hello World!');
